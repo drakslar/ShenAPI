116008191aed776f1a909221f012c28c6759bb34
Zan Drakslar <zdrakslar@gmail.com>
2019-05-14 18:42:32 +0200

    Add Login System

--- ShenAPI.py
+++ ShenAPI.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session, flash
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -11,31 +12,95 @@ db = SQLAlchemy(app)
 
 from models import User
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(user_id)
+
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    if not session.get("logged_in"):
+        return render_template("login.html")
+    else:
+        return render_template("home.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    remember = True if request.form.get("remember") else False
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Please check your login details and try again.")
+        return render_template("login.html")
+    user.authenticated = True
+    db.session.add(user)
+    db.session.commit()
+    login_user(user, remember=remember)
+    session["logged_in"] = True
+    return home()
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        check_email = User.query.filter_by(email=email).first()
+        check_name = User.query.filter_by(name=name).first()
+
+        if (name or email or password or confirm_password) is "":
+            flash("Please check your sign up details and try again.")
+        elif check_email:
+            flash('This email address already exists')
+        elif check_name:
+            flash('This username already exists')
+        elif not (password == confirm_password):
+            flash("The password and confirmation password do not match.")
+        else:
+            encrypted_password = generate_password_hash(request.form.get(password), method="sha256")
+
+            try:
+                user = User(
+                    name = name,
+                    email = email,
+                    password = encrypted_password
+                )
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                login_user(user, remember=True)
+                session["logged_in"] = True
+                home()
+            except Exception as e:
+                return (str(e))
+    return render_template("signup.html")
+
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    session["logged_in"] = False
+    return home()
 
 
 @app.route("/users/<name>")
 def get_user_name(name):
     return "name : {}".format(name)
-
-
-# @app.route('/add')
-# def add_user():
-#     name = request.args.get('name')
-#     password = request.args.get('password')
-#     try:
-#         user = User(
-#             name = name,
-#             password = password
-#         )
-#         db.session.add(user)
-#         db.session.commit()
-#         return "User added. user id = {}".format(user.id)
-#     except Exception as e:
-#         return (str(e))
 
 
 @app.route("/getall")
@@ -56,12 +121,12 @@ def get_by_id(id_):
         return (str(e))
 
 
-@app.route("/add", methods = ['GET', 'POST'])
+@app.route("/add", methods=["GET", "POST"])
 def add_user_form():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        password = request.form.get('password')
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = generate_password_hash(request.form.get("password"), method="sha256")
         try:
             user = User(
                 name = name,
@@ -76,5 +141,18 @@ def add_user_form():
     return render_template("adduser.html")
 
 
-if __name__ == '__main__':
+@app.route("/deleteall")
+@login_required
+def delete_all_users():
+    try:
+        users = User.query.all()
+        for user in users:
+            db.session.delete(user)
+        db.session.commit()
+        return "Deleted all database entries."
+    except Exception as e:
+        return str(e)
+
+
+if __name__ == "__main__":
     app.run()
--- models.py
+++ models.py
@@ -1,18 +1,24 @@
-from ShenAPI import db
+from ShenAPI import db, app
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String())
-    email = db.Column(db.String(), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    email = db.Column(db.String(), unique=True)
     password = db.Column(db.String())
+    authenticated = db.Column(db.Boolean, default=False)
 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
+        self.authenticated = False
 
     def __repr__(self):
         return '<id {}'.format(self.id)
@@ -22,5 +28,18 @@ class User(db.Model):
             'id': self.id,
             'name': self.name,
             'email': self.email,
-            'password': self.password
+            'password': self.password,
+            'authenticated': self.authenticated
         }
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return True
+
+    def is_anonymus(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
